626e1d37f17ae96d5f3b7d8220bb2a2dfa23db8f
Mahinth Joe Christensen <mahinthjoe@gmail.com>
2014-08-02 13:21:56 +0530

    Updated Title Text

--- services.php
+++ services.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Leads Solutions - Products</title>
+    <title>Leads Solutions - Services</title>
     <link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700' rel='stylesheet' type='text/css'>
     <!-- Bootstrap -->
     <link href="_/css/bootstrap.css" rel="stylesheet">
